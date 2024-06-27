0f442112543310549dbafd18a703915d25e39a59
Danyal Ahmed <127227129+scientistdanyal@users.noreply.github.com>
2024-06-27 16:03:45 +0500

    Time: 2 ms (8.06%), Space: 45.2 MB (29.13%) - LeetHub

---
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.java
@@ -0,0 +1,16 @@
+class Solution {
+    public static int[] buildArray(int[] nums) {
+        
+        int mask = 1023;
+        for(int i=0; i < nums.length; i++) {
+            nums[i] = nums[i] | (nums[nums[i]] & mask) << 10;
+            
+        }
+        
+        for(int i=0; i < nums.length; i++) {
+            nums[i] = nums[i] >> 10;
+        }
+        return nums;
+            
+        }
+}
